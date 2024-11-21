d55e13feb96eb83c8bb796077e6c5b8d34a3e992
Vanu122 <99003354+vanu122-Developer@users.noreply.github.com>
2024-11-21 09:52:20 -0800

    Add files via upload

---
+++ Engine-Tools/Engine-Tools/Engine-Tools.cpp
@@ -0,0 +1,15 @@
+﻿// Engine-Tools.cpp : Defines the entry point for the application.
+//
+#include <SDL.h>
+#include "Engine-Tools.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	cout << "Hello CMake." << endl;
+
+	SDL_Quit();
+	return 0;
+}
